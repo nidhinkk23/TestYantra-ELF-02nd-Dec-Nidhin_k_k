2b812f905b92174b8cdcf4e5d8b582c0c1deef2a
nidhinkk23 <58319017+nidhinkk23@users.noreply.github.com>
2019-12-02 13:36:56 -0500

    JavaScript Assessment

---
+++ exam4/exam4.js
@@ -0,0 +1,13 @@
+// var waitTime = 1 * 60 * 1000; // = 30min.
+// console.log(waitTime);
+// var i = 0
+// setInterval(function(){
+    
+//     i++
+//     clearInterval
+//   console.log(i);
+//   if(i === 15){
+//     clearInterval()
+//   }
+  
+// }, 600);
